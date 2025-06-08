189434afb7bd041639482ab3f3265428b39f0278
simone-sampino <s.samp98@gmail.com>
2025-06-08 14:26:30 +0200

    Refactor form structure and add DOM element selections for ticket calculation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("ciao");
+// dom elements
+
+const formEl = document.querySelector("form");
+const inputFullNameEl = document.getElementById("full-name");
+const inputKmEl = document.getElementById("km");
+const selectAgeEl = document.getElementById("age");
+
+console.log(formEl, inputFullNameEl, inputKmEl, selectAgeEl);
